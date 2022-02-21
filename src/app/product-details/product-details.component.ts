f3cf2743288d3ad77fe40c991381ee65cabb18ba
SuchHate <vincent.paradis@hotmail.com>
2022-02-21 14:43:45 -0500

    6 files modified

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
